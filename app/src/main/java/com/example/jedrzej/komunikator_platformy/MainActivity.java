af0b413511613a7034ff83155d2be84ad1f67295
jedrzejkozal <jedrzej.kozal@gmail.com>
2017-04-22 13:26:02 +0200

    Initial commit

---
+++ app/src/main/java/com/example/jedrzej/komunikator_platformy/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.jedrzej.komunikator_platformy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
