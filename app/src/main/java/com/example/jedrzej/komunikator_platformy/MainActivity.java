ca87b4d9ccc32174ef22ef4978ccb4965ab23b93
jedrzejkozal <jedrzej.kozal@gmail.com>
2017-04-22 18:03:25 +0200

    basic GUI project for main activity

--- app/src/main/java/com/example/jedrzej/komunikator_platformy/MainActivity.java
+++ app/src/main/java/com/example/jedrzej/komunikator_platformy/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.jedrzej.komunikator_platformy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.Arrays;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView list ;
+    private ArrayAdapter<String> adapter ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        list = (ListView) findViewById(R.id.listView1);
+
+        String cars[] = {"Mercedes", "Fiat", "Ferrari", "Aston Martin", "Lamborghini", "Skoda", "Volkswagen", "Audi", "Citroen"};
+
+        ArrayList<String> kontakty = new ArrayList<String>();
+        kontakty.addAll( Arrays.asList(cars) );
+
+        adapter = new ArrayAdapter<String>(this, R.layout.row, kontakty);
+
+        list.setAdapter(adapter);
     }
 }
