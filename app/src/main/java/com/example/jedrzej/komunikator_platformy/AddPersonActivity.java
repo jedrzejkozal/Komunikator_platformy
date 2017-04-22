88b0989961ba389b1cd7b102cb6548c5b43fc5e8
jedrzejkozal <jedrzej.kozal@gmail.com>
2017-04-22 19:14:25 +0200

    AddPersonActivity added

---
+++ app/src/main/java/com/example/jedrzej/komunikator_platformy/AddPersonActivity.java
@@ -0,0 +1,18 @@
+package com.example.jedrzej.komunikator_platformy;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by jedrzej on 22.04.17.
+ */
+
+public class AddPersonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_person);
+
+    }
+}
--- app/src/main/java/com/example/jedrzej/komunikator_platformy/MainActivity.java
+++ app/src/main/java/com/example/jedrzej/komunikator_platformy/MainActivity.java
@@ -7,18 +7,23 @@ import java.util.Arrays;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
     private ListView list ;
-    private ArrayAdapter<Person> adapter ;
+    private ArrayAdapter<Person> adapter;
+
+    public Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //creating a new listView
         list = (ListView) findViewById(R.id.listView1);
 
         Person people[] = {new Person("A"),new Person("B"),new Person("C"),new Person("D"),new Person("E"),new Person("F")};
@@ -26,7 +31,16 @@ public class MainActivity extends AppCompatActivity {
         kontakty.addAll( Arrays.asList(people) );
 
         adapter = new ArrayAdapter<Person>(this, R.layout.row, kontakty);
-
         list.setAdapter(adapter);
+
+        //button dodaj kontakt
+        button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainActivity.this, AddPersonActivity.class);
+                //myIntent.putExtra("key",1); //Optional parameters
+                MainActivity.this.startActivity(myIntent);
+            }
+        });
     }
 }
