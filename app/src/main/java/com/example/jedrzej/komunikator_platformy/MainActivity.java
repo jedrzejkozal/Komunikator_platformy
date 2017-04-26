c1fd18fc2aa78901707e211f1f69b29777c84a66
jedrzejkozal <jedrzej.kozal@gmail.com>
2017-04-26 23:07:11 +0200

    Basic activity refactor

--- app/src/main/java/com/example/jedrzej/komunikator_platformy/MainActivity.java
+++ app/src/main/java/com/example/jedrzej/komunikator_platformy/MainActivity.java
@@ -6,18 +6,24 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Button;
 import android.view.View;
 import android.content.Intent;
+import android.widget.TextView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.AdapterView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private ListView list ;
-    private ArrayAdapter<Person> adapter;
+    private ArrayAdapter<String> adapter;
     public Button button;
+    public Button button_send;
+    EditText mEdit;
+    ArrayList<String> conv_history;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,15 +31,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //creating a new listView
-        list = (ListView) findViewById(R.id.listView1);
-        Person people[] = {new Person("A"),new Person("B"),new Person("C"),new Person("D"),new Person("E"),new Person("F")};
-        ArrayList<Person> kontakty = new ArrayList<Person>();
-        kontakty.addAll( Arrays.asList(people) );
+        list = (ListView) findViewById(R.id.listView2);
+        String messages[] = {new String("Me: Hello World"),new String("You: Wanna talk about our Lord and Savior Donald Trump?")};
+        conv_history = new ArrayList<String>();
+        conv_history.addAll( Arrays.asList(messages) );
 
-        adapter = new ArrayAdapter<Person>(this, R.layout.row, kontakty);
+        adapter = new ArrayAdapter<String>(this, R.layout.row, conv_history);
         list.setAdapter(adapter);
 
-        list.setOnItemClickListener(new OnItemClickListener() {
+        /*list.setOnItemClickListener(new OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> arg0, View arg1,int position, long arg3)
             {
@@ -44,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
                 myIntent.putExtra("port",adapter.getItem(position).getPortNumber());
                 MainActivity.this.startActivity(myIntent);
             }
-        });
+        });*/
 
         //button dodaj kontakt
         button = (Button) findViewById(R.id.button1);
@@ -54,5 +60,20 @@ public class MainActivity extends AppCompatActivity {
                 MainActivity.this.startActivity(myIntent);
             }
         });
+
+        //button wyślij
+        mEdit   = (EditText)findViewById(R.id.editText4);
+        button_send = (Button) findViewById(R.id.button2);
+        button_send.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                /*Intent myIntent = new Intent(MainActivity.this, AddPersonActivity.class);
+                MainActivity.this.startActivity(myIntent);*/
+                String tmp = mEdit.getText().toString();
+                conv_history.add("Me: " + tmp);
+                //mEdit.clearComposingText();
+                mEdit.setText("");
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/jedrzej/komunikator_platformy/SendMessageActivity.java
+++ app/src/main/java/com/example/jedrzej/komunikator_platformy/SendMessageActivity.java
@@ -3,13 +3,21 @@ package com.example.jedrzej.komunikator_platformy;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by jedrzej on 22.04.17.
  */
 
 public class SendMessageActivity extends AppCompatActivity {
+
+    private ListView list ;
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +33,16 @@ public class SendMessageActivity extends AppCompatActivity {
             IP = extras.getString("IP");
         }
         TextView textView = (TextView) findViewById(R.id.textView4);
-        textView.setText(name + " " + IP + " " + Integer.toString(port));
+        //textView.setText(name + " " + IP + " " + Integer.toString(port));
+        textView.setText(" ");
+
+        //creating a new listView
+        list = (ListView) findViewById(R.id.listView2);
+        String messages[] = {new String("Me: Hello World"),new String("You: Wanna talk about our Lord and Savior Donald Trump?")};
+        ArrayList<String> conv_history = new ArrayList<String>();
+        conv_history.addAll( Arrays.asList(messages) );
+
+        adapter = new ArrayAdapter<String>(this, R.layout.row, conv_history);
+        list.setAdapter(adapter);
     }
 }
